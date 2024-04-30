97b42276055c0a1a68b413dae724c8f737675e83
AravindMurugan <mi.arvindmurugan@gmail.com>
2024-05-01 00:45:52 +0100

    home page features added

--- src/main/java/com/cafe/hopecafe/homePages/customerHomePageController.java
+++ src/main/java/com/cafe/hopecafe/homePages/customerHomePageController.java
@@ -1,40 +1,125 @@
 package com.cafe.hopecafe.homePages;
 
+import com.cafe.hopecafe.DatabaseConnection;
+import com.cafe.hopecafe.booking.Booking;
+import com.cafe.hopecafe.utils.FxmlPaths;
+import com.cafe.hopecafe.utils.RootBorderPaneHolder;
+import com.cafe.hopecafe.utils.UserData;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.control.Button;
 import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableView;
 import javafx.scene.control.cell.PropertyValueFactory;
+import javafx.scene.layout.AnchorPane;
+import javafx.scene.layout.BorderPane;
 
-import java.time.LocalDate;
-import java.time.LocalTime;
+import java.sql.*;
 import java.util.ArrayList;
 import java.util.List;
 
 public class customerHomePageController {
-//    @FXML
-//    private TableView<Booking> bookingsTable;
-//    @FXML
-//    private TableColumn<Booking, LocalDate> dateColumn;
-//    @FXML
-//    private TableColumn<Booking, LocalTime> timeColumn;
-//    @FXML
-//    private TableColumn<Booking, Integer> guestsColumn;
-//
-//    public void initialize() {
-//        // Create some dummy data
-//        List<Booking> dummyBookings = new ArrayList<>();
-//        dummyBookings.add(new Booking(LocalDate.now(), LocalTime.of(12, 0), 4));
-//        dummyBookings.add(new Booking(LocalDate.now().plusDays(1), LocalTime.of(18, 30), 2));
-//        dummyBookings.add(new Booking(LocalDate.now().plusDays(2), LocalTime.of(10, 0), 6));
-//
-//        // Add the dummy data to the TableView
-//        bookingsTable.getItems().addAll(dummyBookings);
-//
-//        // Set up the cell value factories
-//        dateColumn.setCellValueFactory(new PropertyValueFactory<>("date"));
-//        timeColumn.setCellValueFactory(new PropertyValueFactory<>("time"));
-//        guestsColumn.setCellValueFactory(new PropertyValueFactory<>("guests"));
-//    }
+    @FXML
+    private TableView bookingsTable;
+    @FXML
+    private Button placeEatInOrderButton;
+    @FXML
+    private Button placeTakeawayOrderButton;
+    @FXML
+    private Button requestDeliveryButton;
+    @FXML
+    private Button createBookingButton;
+
+    public void initialize() throws SQLException {
+        List<Booking> bookings = new ArrayList<>();
+        DatabaseConnection connectNow = new DatabaseConnection();
+        Connection connectDB = connectNow.getConnection();
+//        Statement statement = connectDB.createStatement();
+        Integer userId = UserData.getInstance().getUserid();
+        PreparedStatement statement = connectDB.prepareStatement("SELECT * FROM bookings WHERE account_id = ?");
+        statement.setInt(1, userId);
+
+        try(ResultSet resultSet = statement.executeQuery()){
+            while (resultSet.next()) {
+                int bookingId = resultSet.getInt("booking_id");
+                String tableNumber = resultSet.getString("table_number");
+                String date = resultSet.getString("date");
+                String time = resultSet.getString("time");
+                int noOfGuests = resultSet.getInt("no_of_guests");
+                int accountId = resultSet.getInt("account_id");
+                String name = resultSet.getString("name");
+                String email = resultSet.getString("email");
+                String phone = resultSet.getString("phone");
+                Booking booking = new Booking(bookingId, tableNumber, date, time, noOfGuests, accountId, name, email, phone);
+                bookings.add(booking);
+            }
+        }catch (SQLException e) {
+            e.printStackTrace();
+        }
+
+
+
+
+
+// 2. Create an ObservableList from the list of Booking objects
+        ObservableList<Booking> bookingData = FXCollections.observableArrayList(bookings);
+
+// 3. Set the ObservableList as the items for the TableView
+        bookingsTable.setItems(bookingData);
+
+// 4. Bind the TableView columns to the properties of the Booking class
+
+
+        TableColumn<Booking, String> nameColumn = (TableColumn<Booking, String>)
+                bookingsTable.getColumns().get(0);
+        nameColumn.setCellValueFactory(new PropertyValueFactory<>("name"));
+
+        TableColumn<Booking, String> dateColumn = (TableColumn<Booking, String>)
+                bookingsTable.getColumns().get(1);
+        dateColumn.setCellValueFactory(new PropertyValueFactory<>("date"));
+
+        TableColumn<Booking, String> timeColumn = (TableColumn<Booking, String>)
+                bookingsTable.getColumns().get(2);
+        timeColumn.setCellValueFactory(new PropertyValueFactory<>("time"));
+
+        TableColumn<Booking, Integer> noOfGuestsColumn = (TableColumn<Booking, Integer>)
+                bookingsTable.getColumns().get(3);
+        noOfGuestsColumn.setCellValueFactory(new PropertyValueFactory<>("no_of_guests"));
+
+        TableColumn<Booking, Integer> tableNo = (TableColumn<Booking, Integer>)
+                bookingsTable.getColumns().get(4);
+        noOfGuestsColumn.setCellValueFactory(new PropertyValueFactory<>("table_number"));
+
+//        TableColumn<Booking, String> nameColumn = (TableColumn<Booking, String>) bookingsTable.getColumns().get(6);
+//        nameColumn.setCellValueFactory(new PropertyValueFactory<>("name"));
+
+    }
+
+    public void createBookingButtonOnAction(){
+        BorderPane rootPane =RootBorderPaneHolder.getInstance().getRootPane();
+        try {
+            BorderPane fxmlLoader = FXMLLoader.load(getClass().getResource(FxmlPaths.CREATE_BOOKING_FXML));
+            rootPane.getChildren().setAll(fxmlLoader);
+        }catch (Exception e){
+            e.printStackTrace();
+            e.getCause();
+        }
+    }
+
+    public void placeEatInOrderOnAction(){
+        BorderPane rootPane =RootBorderPaneHolder.getInstance().getRootPane();
+        try {
+            AnchorPane fxmlLoader = FXMLLoader.load(getClass().getResource(FxmlPaths.ORDER_MENU_FXML));
+            rootPane.getChildren().setAll(fxmlLoader);
+        }catch (Exception e){
+            e.printStackTrace();
+            e.getCause();
+        }
+    }
+
+
 }
 
 //public class Booking {
